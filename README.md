de5b3154bc87ddad8f4105d9902d021b8ce912b4
Hudson Silva <hudson.davi.cds@gmail.com>
2023-09-21 19:24:35 +0100

    Update Readme

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# Kotlin-Desafio-POO
+# Sistema Educacional em Kotlin
+
+Este é um projeto simples em Kotlin que modela um sistema educacional. Ele permite a criação de bootcamps, formações, inscrições de alunos e o gerenciamento de conteúdos educacionais.
+
+## Conteúdo
+
+- [Visão Geral](#visão-geral)
+- [Como Usar](#como-usar)
+- [Contribuição](#contribuição)
+- [Licença](#licença)
+
+## Visão Geral
+
+Este código implementa um sistema educacional com as seguintes entidades:
+
+- `Nivel`: Uma enumeração que representa os níveis de educação.
+- `Aluno`: Uma classe que representa um aluno com nome e idade.
+- `ConteudoEducacional`: Uma classe que representa um conteúdo educacional com nome, duração e nível.
+- `Bootcamp`: Uma classe que representa um bootcamp com nome e uma lista de conteúdos educacionais. Ela permite a matrícula de alunos.
+- `Formacao`: Uma classe que representa uma formação com nome e uma lista de conteúdos educacionais. Ela também permite a matrícula de alunos.
+
+O código cria instâncias dessas classes e demonstra como matricular alunos em bootcamps e formações com diferentes conteúdos educacionais.
+
+## Como Usar
+
+1. Clone este repositório:
+
+```bash
+git clone https://github.com/HudDavi/Kotlin-Desafio-POO.git
+```
+
+2. Navegue até a pasta do projeto:
+
+```bash
+cd Kotlin-Desafio-POO
+```
+
+3. Execute o programa:
+
+```bash
+kotlin main.kt
+```
+
+Siga as instruções no console para criar bootcamps, formações, adicionar conteúdos educacionais e matricular alunos.
+
+## Contribuição
+
+Se você deseja contribuir para este projeto, siga as etapas abaixo:
+
+1. Fork o projeto.
+2. Crie uma nova branch com a sua funcionalidade: `git checkout -b feature/nova-funcionalidade`.
+3. Faça commit das suas alterações: `git commit -am 'Adicionei uma nova funcionalidade'`.
+4. Faça push para a branch: `git push origin feature/nova-funcionalidade`.
+5. Abra uma solicitação pull no GitHub.
+
+## Licença
+
+Este projeto é licenciado sob a [Licença MIT](LICENSE).
--- main.kt
+++ main.kt
@@ -1,34 +1,39 @@
 enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Aluno{
+// Classe que representa um aluno com nome e idade
+class Aluno {
     val nome: String
     val idade: Int
 
-    constructor(nome: String, idade: Int){
+    constructor(nome: String, idade: Int) {
         this.nome = nome
         this.idade = idade
     }
 }
 
+// Classe que representa um conteúdo educacional com nome, duração e nível
 data class ConteudoEducacional constructor(val nome: String, val duracao: Int, val nivel: Nivel)
 
-data class Bootcamp constructor(val nome: String, val conteudos: List<ConteudoEducacional>){
+// Classe que representa um bootcamp com nome e uma lista de conteúdos educacionais
+data class Bootcamp constructor(val nome: String, val conteudos: List<ConteudoEducacional>) {
     val inscritos = mutableListOf<Aluno>()
 
-    fun matricular(aluno: Aluno){
+    fun matricular(aluno: Aluno) {
         inscritos.add(aluno)
     }
 }
 
-data class Formacao constructor(val nome: String, val conteudos: List<ConteudoEducacional>){
+// Classe que representa uma formação com nome e uma lista de conteúdos educacionais
+data class Formacao constructor(val nome: String, val conteudos: List<ConteudoEducacional>) {
     val inscritos = mutableListOf<Aluno>()
 
-    fun matricular(aluno: Aluno){
+    fun matricular(aluno: Aluno) {
         inscritos.add(aluno)
     }
 }
 
 fun main() {
+    // Criando alguns conteúdos educacionais
     val conteudoJavaBasico = ConteudoEducacional("Java Básico", 60, Nivel.BASICO)
     val conteudoJavaIntermediario = ConteudoEducacional("Java Intermediário", 120, Nivel.INTERMEDIARIO)
     val conteudoJavaAvancado = ConteudoEducacional("Java Avançado", 180, Nivel.AVANCADO)
@@ -37,15 +42,18 @@ fun main() {
     val conteudoKotlinIntermediario = ConteudoEducacional("Kotlin Intermediário", 120, Nivel.INTERMEDIARIO)
     val conteudoKotlinAvancado = ConteudoEducacional("Kotlin Avançado", 180, Nivel.AVANCADO)
 
+    // Criando bootcamps e formações com esses conteúdos educacionais
     val bootcampJava = Bootcamp("Java", listOf(conteudoJavaBasico))
     val bootcampKotlin = Bootcamp("Kotlin", listOf(conteudoKotlinBasico))
 
     val formacaoJava = Formacao("Java", listOf(conteudoJavaBasico, conteudoJavaIntermediario, conteudoJavaAvancado))
     val formacaoKotlin = Formacao("Kotlin", listOf(conteudoKotlinBasico, conteudoKotlinIntermediario, conteudoKotlinAvancado))
 
+    // Criando alguns alunos
     val aluno1 = Aluno("Hudson", 30)
     val aluno2 = Aluno("Davi", 30)
 
+    // Matriculando os alunos nos bootcamps e formações
     bootcampJava.matricular(aluno1)
     formacaoJava.matricular(aluno1)
 
